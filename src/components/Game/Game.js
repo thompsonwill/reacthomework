4607cf00b507c18e91f5eab63d5a9ff26c8deac5
Will <williamjamesthompson@gmail.com>
2018-10-11 12:31:51 -0500

    Got the people to shufft around

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,38 +5,56 @@ import people from "../../people.json";
 import Title from "../Title"
 import Header from "../Header";
 
+// Create Random order of people from the array
+function randomPeople(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 class Game extends React.Component {
 
-    state = {
-        score: 0
-    };
-  
-  
-    handleClick = (id) => {
-      this.setState({ score: this.state.score + 1 });
-    };
-  
-  
-  
-  
-  
-    render() {
-      return (
-        <Wrapper>
-          <Title>Hello, React Homework</Title>
-          <Header></Header>
-          {people.map(people => (
-            <PeopleCard
-              id={people.id}
-              handleClick={this.handleClick}
-              key={people.id}
-              name={people.name}
-              image={people.image}
-            />
-          ))}
-        </Wrapper>
-      );
-    }
+  state = {
+    people,
+    currectScore: 0,
+    highScore: 0
+  };
+
+
+  handleShuffle = () => {
+    let shufflePeople = randomPeople(people);
+    this.setState({ people: shufflePeople });
   }
-  
-  export default Game;
+
+  // Testing updating the state
+  handleClick = (id) => {
+    this.setState({ score: this.state.score + 1 });
+    this.handleShuffle();
+  };
+
+
+
+
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Hello, React Homework</Title>
+        <Header></Header>
+        {people.map(people => (
+          <PeopleCard
+            id={people.id}
+            handleClick={this.handleClick}
+            key={people.id}
+            name={people.name}
+            image={people.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default Game;
